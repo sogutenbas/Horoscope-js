5df12d6c0e1e08634d8a26e3f1f5043e6a13ebe1
serhat oguten <sogutenbas@gmail.com>
2021-02-16 22:00:14 +0100

    first commit

---
+++ horoscope.js
@@ -0,0 +1,39 @@
+let month = 10
+let day = 6
+
+if ((month==3 && day>20)||(month==4 && day<21)) {
+    console.log("Koc Burcu")
+}
+else if ((month==4 && day>20)||(month==5 && day<22)) {
+    console.log("Boga Burcu")
+}
+else if ((month==5 && day>21)||(month==6 && day<23)) {
+    console.log("Ikizler Burcu")
+}
+else if ((month==6 && day>22)||(month==7 && day<23)) {
+    console.log("Yengec Burcu")
+}
+else if ((month==7 && day>22)||(month==8 && day<23)) {
+    console.log("Aslan Burcu")
+}
+else if ((month==8 && day>22)||(month==9 && day<23)) {
+    console.log("Basak Burcu")
+}
+else if ((month==9 && day>22)||(month==10 && day<23)) {
+    console.log("Terazi Burcu")
+}
+else if ((month==10 && day>22)||(month==11 && day<22)) {
+    console.log("Akrep Burcu")
+}
+else if ((month==11 && day>21)||(month==12 && day<22)) {
+    console.log("Yay Burcu")
+}
+else if ((month==12 && day>21)||(month==1 && day<22)) {
+    console.log("Oglak Burcu")
+}
+else if ((month==1 && day>21)||(month==2 && day<20)) {
+    console.log("Kova Burcu")
+}
+else {
+    console.log("Balik Burcu")
+}
